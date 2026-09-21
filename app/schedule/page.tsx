b5eb2b8260cf037b36fb3ce962a7ff1e87c22dd6
17hiroki17 <hiroyuzu1011@gmail.com>
2026-09-21 20:14:15 +0900

    Update page.tsx

--- app/schedule/page.tsx
+++ app/schedule/page.tsx
@@ -97,6 +97,20 @@ export default async function SchedulePage({
         </p>
       </div>
 
+      <div className="mb-6 rounded border border-amber-300 bg-amber-50 p-4">
+        <h2 className="mb-2 font-semibold">【注意事項】</h2>
+
+        <ul className="list-disc pl-5 space-y-1">
+          <li>食事は禁止です。</li>
+          <li>印刷を行う場合は、2号館のパソコンをご利用ください。</li>
+          <li>施設利用の都合上、閉校時間は21:30です。</li>
+          <li>東進衛星予備校 刈谷駅南口校に在籍している生徒のみ利用可能です。</li>
+          <li>
+            他の利用者の迷惑となる行為やマナー違反があった場合は、今後の利用をお断りする場合があります。
+          </li>
+        </ul>
+      </div>
+
       <div>
         <h2 className="mb-2 font-semibold">開放教室</h2>
 
